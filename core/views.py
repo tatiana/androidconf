fcd6e8842a31c91385af7e5671f68b458cc50817
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2011-09-11 18:15:00 -0300

    ADD: GoogleMaps widget and appropriate links pointing to templates

--- core/views.py
+++ core/views.py
@@ -1,6 +1,18 @@
+from django.conf import settings
 from django.shortcuts import render_to_response
 
-def homepage(request):
-    from django.conf import settings
+def about(request):
     context = {'STATIC_URL': settings.STATIC_URL}
     return render_to_response('index.html', context)
+
+def place(request):
+    context = {'STATIC_URL': settings.STATIC_URL}
+    return render_to_response('place.html', context)
+
+def subscribe(request):
+    context = {'STATIC_URL': settings.STATIC_URL}
+    return render_to_response('under-construction.html', context)
+
+def schedule(request):
+    context = {'STATIC_URL': settings.STATIC_URL}
+    return render_to_response('under-construction.html', context)
--- urls.py
+++ urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls.defaults import patterns
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from core.views import homepage
+from core.views import about, place, subscribe, schedule
 
 urlpatterns = patterns('',
-                 (r'^$', homepage),
-)
+                 (r'^index.html$', about),
+                 (r'^sobre.html$', about),
+                 (r'^local.html$', place),
+                 (r'^inscricao.html$', subscribe),
+                 (r'^programacao.html$', schedule),
+                 (r'^$', about),
+                 (r'^.*$', about),)
 
 urlpatterns += staticfiles_urlpatterns()
