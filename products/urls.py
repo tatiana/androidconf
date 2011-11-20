0bec2282c9c8181ebe3d760da99868bacc811a19
Tatiana Al-Chueyr Martins <tatiana.alchueyr@gmail.com>
2011-11-20 01:59:00 -0600

    FIX: Some fixtures from server side related to URLs

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
-    url(r'2', 'androidconf.products.views.product_detail_2nd', name='product_detail_2nd'),
-    url(r'$', 'androidconf.products.views.product_detail', name='product_detail'),
+    url(r'^$', 'androidconf.products.views.product_detail_2nd', name='product_detail_2nd'),
+    url(r'lista-de-espera$', 'androidconf.products.views.product_detail', name='product_detail'),
 )
 
